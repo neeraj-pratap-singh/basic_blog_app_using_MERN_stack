db424d10be0f7b48ccf9c3be00a73b7d530b2c7a
neeraj-singh <71137350+neeraj-pratap-singh@users.noreply.github.com>
2024-02-17 15:11:29 +0530

    restructure the backend code

---
+++ Backend/config/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    jwtSecret: 'f47IDRxbQpeyBawzPeZzP6v2vtNYiQQSm3ZqV/5raaM=',
+    mongodbURI: 'mongodb+srv://neeraj:neeraj1421@cluster0learn.muek4hy.mongodb.net/blog_app'
+};
--- config/config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    jwtSecret: 'your_jwt_secret_here',
-    mongodbURI: 'mongodb://localhost:27017/your_database_name'
-};
